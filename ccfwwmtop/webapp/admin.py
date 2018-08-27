45b2bc1039d9df2907a7c419858231e1ed23b296
ccfwwm <ccfwwm@gmail.com>
2018-08-27 17:26:07 +0800

    get ip to diff dev or prodect

--- ccfwwmtop/webapp/admin.py
+++ ccfwwmtop/webapp/admin.py
@@ -2,8 +2,25 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Question,Choice
+from .models import Question, Choice
 
 
-admin.site.register(Question)
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('问题内容', {'fields': ['question_text']}),
+        ('问题信息', {'fields': ['pub_date'],'classes':['collapse']})
+       # ('问题信息', {'fields': ['pub_date']})
+    ]
+    inlines = [ChoiceInLine]
+    list_display = ['question_text','pub_date', 'was_published_recently']
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
